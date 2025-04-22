2e50f105b9b1f3f8161fe4e5f4090f3f8949aae6
zsj <2879979437@qq.com>
2025-04-22 17:34:15 +0800

    项目初始

---
+++ src/main/java/generator/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 86157
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-04-22 17:32:31
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
